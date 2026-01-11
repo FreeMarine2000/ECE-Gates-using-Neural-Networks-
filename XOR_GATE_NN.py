cbdb574d0ebccf4452c6241beeef0ae7bedf7504
FreeMarine2000 <180565177+FreeMarine2000@users.noreply.github.com>
2026-01-11 18:13:25 +0530

    doubled neural layer for accuracy

--- XOR_GATE_NN.py
+++ XOR_GATE_NN.py
@@ -1,19 +1,23 @@
 import tensorflow as tf
 import numpy as np
 import os 
+import time
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 print(f"TensorFlow Version: {tf.__version__}")
 X = np.array([[0,0], [0,1], [1,0], [1,1]], dtype="float32")
 y = np.array([[0], [1], [1], [0]], dtype="float32")
 model = tf.keras.Sequential([
-    tf.keras.layers.Dense(8, input_dim=2, activation='relu'),
+    tf.keras.layers.Dense(16, input_dim=2, activation='relu'),
     tf.keras.layers.Dense(1, activation='sigmoid')
 ])
 model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.1),
               loss='binary_crossentropy',
               metrics=['accuracy'])
-print("running it 500 x")
-model.fit(X, y, epochs=500, verbose=0) 
+print("running it nfinite x")
+start_time = time.time()
+model.fit(X, y, epochs=2000, verbose=0) 
+end_time = time.time()
+print(f"Training time: {end_time - start_time:.4f} seconds")
 print("Training complete.")
 print("predict")
 predictions = model.predict(X)
